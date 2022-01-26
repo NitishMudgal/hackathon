ad85a023f5d6704651aabd99e2b399d6581b13ff
Nitish Mudgal <nitish.mudgal@organic.ly>
2022-01-26 10:38:59 -0500

    running app

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 from app.forms import SearchForm
 import time
@@ -7,36 +7,18 @@ import json
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = SearchForm()
-    if form.validate_on_submit():
-        # return f'''<h1> Welcome {form.query_string.data} </h1>'''
+    if request.method == "POST":
+       query_string = request.form.get("queryString")
+       if query_string:
         start = time.time()
-        print('Start Time')
-        print(start)
         final_list = []
-        raw_data = [json.loads(line) for line in open('/Users/nitishmudgal/Organic/hackathon/empirio_tmp.json', 'r')]
-        # data1 = json.load(json.dumps([f]))
-        # print(raw_data)
+        raw_data = [json.loads(line) for line in open('/Users/nitishmudgal/Organic/hackathon/empireio_tmp_content_first_sentence.json', 'r')]
+        print('RAW DATA length')
+        print(len(raw_data))
         for row in raw_data:
-            if form.query_string.data in row['title']:
+            if query_string in row['title']:
                 final_list.append(row)
-        # print(final_list)
         end = time.time()
-        print('ENd time')
-        print(end)
         processing_time = round((end - start)*1000)
-        print('processing time')
-        print(processing_time)
-        print(type(processing_time))
-        return render_template('results.html', final_list=final_list, processing_time=processing_time)
-    return render_template('index.html', form=form)
-    # return render_template('index.html', title='Home', user=user)
-
-@app.route('/index2', methods=['GET', 'POST'])
-def index2():
-    return render_template('index2.html')
-
-@app.route('/index3', methods=['GET', 'POST'])
-def index3():
-    return render_template('index3.html')
-
+        return render_template('results.html', final_list=final_list, processing_time=processing_time, query_string=query_string)
+    return render_template('index.html')
